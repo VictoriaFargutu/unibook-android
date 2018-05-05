bc2383d8e75f6b302649c6f7b3d250b1ea2bb446
victoria.fargutu <fargutu.victoria@gmail.com>
2018-05-05 12:50:55 +0300

    Recycler view and list xml

--- app/src/main/java/com/fargutuvictoria/unibook/commons/TabConstants.java
+++ app/src/main/java/com/fargutuvictoria/unibook/commons/TabConstants.java
@@ -7,6 +7,6 @@ package com.fargutuvictoria.unibook.commons;
 public class TabConstants {
     public static final int HOME_COURSE_TAB_POSITION = 0;
     public static final int HOME_SEM_TAB_POSITION = 1;
-    public static final int HOME_LAB_TAB_POSITION = 3;
+    public static final int HOME_LAB_TAB_POSITION = 2;
     public static final int HOME_TABS_COUNT = 3;
 }
--- app/src/main/java/com/fargutuvictoria/unibook/ui/home/classroom/CourseRoomFragment.java
+++ app/src/main/java/com/fargutuvictoria/unibook/ui/home/classroom/CourseRoomFragment.java
@@ -1,6 +1,7 @@
 package com.fargutuvictoria.unibook.ui.home.classroom;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,7 @@ import com.fargutuvictoria.unibook.R;
 public class CourseRoomFragment extends android.support.v4.app.Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_course_rooms_list, container, false);
     }
 }
