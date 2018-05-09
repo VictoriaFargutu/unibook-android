dc8b572c8d71e1e21006d00d5a96363c80692249
victoria.fargutu <fargutu.victoria@gmail.com>
2018-05-09 23:16:28 +0300

    Added Filters layout

---
+++ app/src/main/java/com/fargutuvictoria/unibook/ui/filter/ReservationFilterActivity.java
@@ -0,0 +1,15 @@
+package com.fargutuvictoria.unibook.ui.filter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fargutuvictoria.unibook.R;
+
+public class ReservationFilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_filter);
+    }
+}
