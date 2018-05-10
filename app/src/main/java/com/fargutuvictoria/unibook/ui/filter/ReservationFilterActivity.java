0908dab5643ed1761b490ec5aeac19da822ab44d
victoria.fargutu <fargutu.victoria@gmail.com>
2018-05-10 23:35:46 +0300

    + small changes to Filters layout

--- app/src/main/java/com/fargutuvictoria/unibook/ui/filter/ReservationFilterActivity.java
+++ app/src/main/java/com/fargutuvictoria/unibook/ui/filter/ReservationFilterActivity.java
@@ -1,9 +1,17 @@
 package com.fargutuvictoria.unibook.ui.filter;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.fargutuvictoria.unibook.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class ReservationFilterActivity extends AppCompatActivity {
 
@@ -11,5 +19,30 @@ public class ReservationFilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation_filter);
+
+        final EditText dateEditText = findViewById(R.id.date_picker);
+        final Calendar calendar = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                String myFormat = "dd/MM/yy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                dateEditText.setText(sdf.format(calendar.getTime()));
+            }
+        };
+
+        dateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(ReservationFilterActivity.this, date, calendar
+                        .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/fargutuvictoria/unibook/ui/home/HomeActivity.java
+++ app/src/main/java/com/fargutuvictoria/unibook/ui/home/HomeActivity.java
@@ -2,23 +2,48 @@ package com.fargutuvictoria.unibook.ui.home;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.fargutuvictoria.unibook.R;
 import com.fargutuvictoria.unibook.ui.home.adapter.HomeFragmentPagerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
     private TabLayout tabLayout;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        setContentView(R.layout.activity_home);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerLayout = findViewById(R.id.drawerr_layout);
+
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+//            @Override
+//            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                // set item as selected to persist highlight
+//                item.setChecked(true);
+//                // close drawer when item is tapped
+//                drawerLayout.closeDrawers();
+//
+//                // Add code here to update the UI based on the item selected
+//                // For example, swap UI fragments here
+//
+//                return true;
+//            }
+//        });
 
         // Find the view pager that will allow the user to swipe between fragments
         ViewPager viewPager = findViewById(R.id.home_view_pager);
@@ -34,4 +59,20 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
 }
